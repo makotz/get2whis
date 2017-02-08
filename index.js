2422c8d6c4b3336ae7bdd23699feb9946b45ef8c
Makoto Ejima <makotoejima@gmail.com>
2017-02-08 10:09:35 -0800

    quickies

--- index.js
+++ index.js
@@ -186,6 +186,23 @@ function receivedMessage(event) {
   }
 }
 
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback
+  // button for Structured Messages.
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " +
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to
+  // let them know it was successful
+  sendTextMessage(senderID, "Postback called");
+}
+
 function askDriveOrRide(recipientId) {
   var messageData = {
     recipient: {
@@ -293,7 +310,6 @@ function askDepartureTime(recipientId, drivingOrRiding, departureLocation) {
   callSendAPI(messageData);
 }
 
-
 function sendTextMessage(recipientId, messageText) {
 
   var messageData = {
@@ -308,7 +324,6 @@ function sendTextMessage(recipientId, messageText) {
 
   callSendAPI(messageData);
 }
-
 
 function sendDriveOrRide(recipientId) {
   var messageData = {
