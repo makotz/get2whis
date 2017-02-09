c4848e65ad520ecc3751ad0ab40c20a0001d6519
Makoto Ejima <makotoejima@gmail.com>
2017-02-08 20:18:55 -0800

    pipikaula

--- index.js
+++ index.js
@@ -133,9 +133,9 @@ function receivedMessage(event) {
         // keywords and send back the corresponding example. Otherwise, just echo
         // the text we received.
         switch (messageText) {
-            case 'topsecret':
-                sendDriveOrRide(senderID);
-                break;
+            // case 'topsecret':
+            //     sendDriveOrRide(senderID);
+            //     break;
 
             case 'aloha':
                 askDriveOrRide(senderID);
