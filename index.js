249bae5fd77332f06a94053c673aa6cd6010ff66
Admin <admin@Admins-MacBook-Air-29.local>
2017-02-24 15:51:50 -0800

    trying saveAndQuery v4

--- index.js
+++ index.js
@@ -540,7 +540,7 @@ function saveAndQuery(sender, conditions, userProfile) {
       pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('INSERT INTO rider (sender_id, first_name, last_name, profile_pic, gender, departure_location, departure_date, departure_time) values($1, $2, $3, $4, $5, $6, $7, $8)', [sender, wholeProfile.first_name, wholeProfile.last_name, wholeProfile.profile_pic, wholeProfile.gender, wholeProfile.departure_location, wholeProfile.departure_date, wholeProfile.departure_time]);
         var potentialDriver = client.query('SELECT (first_name, last_name, profile_pic, departure_date, departure_time) FROM driver WHERE departure_location ='+wholeProfile.departure_location+'AND departure_date = '+wholeProfile.departure_date+'AND departure_time = '+wholeProfile.departure_time)
-        console.log("potentialRiders are "+JSON.parse(potentialDriver));
+        console.log("potentialRiders are "+JSON.stringify(potentialDriver));
       });
     }
 };
