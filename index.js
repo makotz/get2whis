48d5b3480478899e3c309f96d075bf6d99e5d1f5
Makoto Ejima <makotoejima@gmail.com>
2017-03-23 23:55:11 -0700

    fix JSON

--- index.js
+++ index.js
@@ -557,7 +557,7 @@ function pingPostOwner(senderId, payload) {
                 }, {
                     type: "postback",
                     title: "Found a 🚗  already...",
-                    payload: "{deleteQuery: true, driverOrRiderTable: driver, postId: " + payload.match_rider_id + ", sender_id: " + payload.sender_id + ", match_first_name: " + payload.match_first_name + ", senderId: " + senderId + ",first_name: " + payload.first_name + "}"
+                    payload: '{deleteQuery: true, "driverOrRiderTable": "driver", "postId": ' + payload.match_rider_id + ', "sender_id": ' + payload.sender_id + ', "match_first_name": "' + payload.match_first_name + '", "senderId": ' + senderId +',"first_name": "' + payload.first_name + '"}'
                 }
             ]
         }
